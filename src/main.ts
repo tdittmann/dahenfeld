f102a82f8403a80bfb6905ac4149112d6fa70bf5
Timo Dittmann <timo.dittmann@mail.schwarz>
2025-09-18 21:45:46 +0200

    Some improvements

--- src/main.ts
+++ src/main.ts
@@ -25,12 +25,5 @@ import { IonicVue } from "@ionic/vue";
 const app = createApp(App).use(createPinia()).use(router).use(IonicVue);
 
 router.isReady().then(() => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(
-      new URL("./serviceworker/firebase-messaging-sw.ts", import.meta.url),
-      { type: "module" },
-    );
-  }
-
   app.mount("#app");
 });

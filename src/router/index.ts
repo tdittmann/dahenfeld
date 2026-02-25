de64fb9a83b1fb3120e362786c5c910efcf66f52
Timo Dittmann <tdittmann@users.noreply.github.com>
2026-02-25 13:36:25 +0100

    Fix base (#76)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "@ionic/vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/"),
   routes: [
     {
       path: "/",
